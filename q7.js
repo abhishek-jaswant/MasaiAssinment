eab3a50239b58ed1a5af07ee948376a7c4798d4e
abhishek-jaswant <ahirwarabhishek859@gmail.com>
2025-01-05 15:04:07 +0530

    i used function

---
+++ q7.js
@@ -0,0 +1,17 @@
+function printMasaiSchool(L1,B1,L2,B2){
+    var area1 = L1 * B1;
+    var area2 = L2 * B2;
+    var pramiter1 = 2 * (L1 + B1);
+    var pramiter2 = 2 * (L2 + B2);
+    if(area1 > area2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+   if(pramiter1 > pramiter2){
+       console.log("Yes");
+   }else{
+       console.log("No");
+   }
+  
+}
