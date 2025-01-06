67dbdccd58c1f061301e0ef5efeff3216f78d7b4
abhishek-jaswant <ahirwarabhishek859@gmail.com>
2025-01-06 09:21:23 +0530

    intrest

---
+++ Problem4_SimpleInterestCalculator.js
@@ -0,0 +1,7 @@
+const p=prompt("Enter the principal");
+const R=prompt("Enter the Rate");
+const T=prompt("Enter the Time");
+const  interest=(P*R*T)/100;
+
+// "The simple interest is: 525..0"
+console.log("The simple interest is: ",interest)
