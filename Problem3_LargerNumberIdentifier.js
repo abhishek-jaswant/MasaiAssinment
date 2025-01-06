5d18c1f9d3690d3fc8b16707d736c8b5c4ed246f
abhishek-jaswant <ahirwarabhishek859@gmail.com>
2025-01-06 09:11:43 +0530

    LargNumberFind

---
+++ Problem3_LargerNumberIdentifier.js
@@ -0,0 +1,11 @@
+const number1=prompt("Enter first number");
+const number2=prompt("Enter the second number");
+if(number1>number2){
+    console.log(number1,"is greater then ",number2)
+}
+else if(number2>number1){
+    console.log(number2,"is greater then ",number1)
+}
+else{
+    console.log("Both are equal");
+}
