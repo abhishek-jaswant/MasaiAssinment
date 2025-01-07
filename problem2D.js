8a57e1a13a30b272f3d9328775d3f7b050bd6fb3
abhishek-jaswant <ahirwarabhishek859@gmail.com>
2025-01-07 16:10:06 +0530

    maltidaimensnal

---
+++ problem2D.js
@@ -0,0 +1,11 @@
+let arr=[[1,2],[3,4],[5,6]]; 
+for(let i=0;i<arr.length;i++)
+{        let bar=" ";
+        for(let j=0;j<arr[i].length;j++)
+        {
+         
+          bar+=arr[i][j]+" ";
+         
+        } 
+        console.log(bar);
+}
